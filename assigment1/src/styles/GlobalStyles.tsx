9cb5d385597165364a6f7fb8f5d9b90e6564c483
Aleksandra289 <avujovic737@gmail.com>
2024-01-15 15:17:40 +0100

    global styles added

---
+++ assigment1/src/styles/GlobalStyles.tsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles= createGlobalStyle`
+    *{
+        box-sizing: border-box;
+    }
+    body{
+        font-family: "Inter", sans-serif;
+        margin: 0;
+    }
+`
+
+
+export default GlobalStyles;
