365e3db637f3102ca31df33fc9742d42f9d97090
Aleksandra289 <avujovic737@gmail.com>
2024-01-23 09:04:52 +0100

    navitem-fixed

--- assigment1/src/components/NavItem/StyledNavItem.ts
+++ assigment1/src/components/NavItem/StyledNavItem.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledNavItemDiv= styled.div<{isActive: boolean}>`
-display: flex;
+display: inline-flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
