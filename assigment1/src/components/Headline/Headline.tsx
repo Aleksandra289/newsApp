dd6830f78a30ec2c9d647608ddd3696df8eb6bb5
Aleksandra289 <avujovic737@gmail.com>
2024-01-24 11:42:05 +0100

    headline-fixed

--- assigment1/src/components/Headline/Headline.tsx
+++ assigment1/src/components/Headline/Headline.tsx
@@ -3,15 +3,15 @@ import { StyledDiv, StyledBorder } from "./StyledHeadline";
 
 type HeadlineProps={
     title: string;
-    titleBelow?: string
+    isAddedLineBelow?: boolean
 }
 
-function Headline({title, titleBelow=title}: HeadlineProps){
+function Headline({title, isAddedLineBelow=true}: HeadlineProps){
     return <StyledDiv>
         <Text fontSize="large" fontWeight="extraBold" lineHeight="high" color="lightBlack">{title}</Text>
         <StyledBorder/>
         <StyledDiv>
-            <Text fontSize="large" fontWeight="extraBold" lineHeight="high" color="lightBlack">{titleBelow}</Text>
+            { isAddedLineBelow && <Text fontSize="large" fontWeight="extraBold" lineHeight="high" color="lightBlack">{title}</Text>}
         </StyledDiv>
     </StyledDiv>
 }
