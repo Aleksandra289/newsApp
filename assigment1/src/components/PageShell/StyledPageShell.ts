41e4dd5ead78ec212f380c9fc41cc47c92625671
Aleksandra289 <avujovic737@gmail.com>
2024-01-29 19:34:40 +0100

    layout-and-pageshell-fixed

--- assigment1/src/components/PageShell/StyledPageShell.ts
+++ assigment1/src/components/PageShell/StyledPageShell.ts
@@ -4,5 +4,14 @@ export const StyledPageShellDiv= styled.main`
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-    gap: 24px;
+    gap: 1.5rem;
+    width: 77.5rem;
+
+    @media screen and (max-width: 46.5rem){
+        display: flex;
+        width: 44rem;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 1.5rem
+    }
 `
--- assigment1/src/layout/StyledLayout.ts
+++ assigment1/src/layout/StyledLayout.ts
@@ -7,8 +7,17 @@ export const StyledLayoutNavDiv= styled.header`
 export const StyledLayoutDiv= styled.div`
     display: flex;
     width: 100%;
-    padding-bottom: 36px;
+    padding-bottom: 2.25rem;
     flex-direction: column;
     align-items: center;
-    gap: 24px;
+    gap: 1.5rem;
+
+    @media screen and (max-width: 46.5rem){
+        display: flex;
+        width: 100%;
+        padding-bottom: 2.5rem;
+        flex-direction: column;
+        align-items: center;
+        gap: 1.25rem;
+    }
 `
