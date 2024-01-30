591d3e2e9cf39e07cd48bb05456406584d08ae21
Aleksandra289 <avujovic737@gmail.com>
2024-01-30 14:31:02 +0100

    setup-application

--- assigment1/src/layout/Layout.tsx
+++ assigment1/src/layout/Layout.tsx
@@ -1,16 +1,26 @@
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ItemProps } from "../shared/types/navItem";
 import { items } from "../shared/data/navigation/items";
 import Navigation from "../components/Navigation/Navigation";
 import { useNavigate } from "react-router-dom";
 import PageShell from "../components/PageShell/PageShell";
-import { StyledLayout } from "./StyledLayout";
-import { StyledPageShellWrapper } from "../components/PageShell/StyledPageShell";
+import { StyledLayoutDiv } from "./StyledLayout";
+import { StyledPageShellWrapperDiv } from "../components/PageShell/StyledPageShell";
+import { useLocation } from "react-router-dom";
 
 export default function Layout() {
   const [activeItem, setActiveItem] = useState<ItemProps>(items[0]);
+
   const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const selectedItem = items.find((item) => item.url === location.pathname);
+    if (selectedItem) {
+      setActiveItem(selectedItem);
+    }
+  }, [location.pathname]);
 
   function onChangeActiveNavItem(selectedItem: ItemProps) {
     setActiveItem(selectedItem);
