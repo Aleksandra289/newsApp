ab9a412e34075de41acc07b269ee2203eb289cf9
Aleksandra289 <avujovic737@gmail.com>
2024-02-05 16:06:36 +0100

    post-form-created

--- assigment1/src/components/Button/Button.tsx
+++ assigment1/src/components/Button/Button.tsx
@@ -10,19 +10,26 @@ type ButtonProps = {
   isDisabled?: boolean;
   width?: string;
   onClick?: () => void;
+  type?: "button" | "submit" | "reset";
 };
 
 function ButtonComponent({
   children,
   size,
   isDisabled = false,
-  onClick
+  onClick,
+  type = "button"
 }: ButtonProps) {
   const textColor = isDisabled ? "darkGray" : "white";
   const [fSize, fWeight, lHeight] = getTextSizes(size);
 
   return (
-    <StyledButton size={size} $isDisabled={isDisabled} onClick={onClick}>
+    <StyledButton
+      size={size}
+      type={type}
+      $isDisabled={isDisabled}
+      onClick={onClick}
+    >
       <Text
         fontSize={fSize}
         fontWeight={fWeight}
--- assigment1/src/components/Input/Input.tsx
+++ assigment1/src/components/Input/Input.tsx
@@ -5,79 +5,79 @@ import {
   StyledErrorDiv
 } from "./StyledInput";
 import Text from "../Text/Text";
-import { useState, InputHTMLAttributes } from "react";
+import { useState, InputHTMLAttributes, forwardRef, Ref } from "react";
 
 interface InputType extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   error?: string;
-  value: string;
+  value?: string;
   disabled?: boolean;
 }
 
-function Input({
-  label,
-  error = "",
-  value,
-  disabled = false,
-  ...restProps
-}: InputType) {
-  const [isFocused, setIsFocused] = useState(false);
+const Input = forwardRef(
+  (
+    { label, error = "", value, disabled = false, ...restProps }: InputType,
+    ref: Ref<HTMLInputElement>
+  ) => {
+    const [isFocused, setIsFocused] = useState(false);
 
-  function onFocusHandler() {
-    setIsFocused(true);
+    function onFocusHandler() {
+      setIsFocused(true);
+    }
+
+    function onBlurHandler() {
+      setIsFocused(false);
+    }
+
+    return (
+      <StyledInputDiv>
+        <StyledTextDiv>
+          {isFocused && !disabled && (
+            <Text
+              fontSize="xsmall"
+              fontWeight="regular"
+              lineHeight="low"
+              color={error ? "redish" : "purple"}
+            >
+              {label}
+            </Text>
+          )}
+          {!isFocused && value && !error && !disabled && (
+            <Text
+              fontSize="xsmall"
+              fontWeight="regular"
+              lineHeight="low"
+              color="silver"
+            >
+              {label}
+            </Text>
+          )}
+        </StyledTextDiv>
+        <StyledInput
+          type="text"
+          onFocus={onFocusHandler}
+          onBlur={onBlurHandler}
+          $error={error}
+          disabled={disabled}
+          $isFocused={isFocused}
+          ref={ref}
+          {...restProps}
+        />
+
+        <StyledErrorDiv>
+          {error && (
+            <Text
+              fontSize="xsmall"
+              fontWeight="regular"
+              lineHeight="low"
+              color="redish"
+            >
+              {error}
+            </Text>
+          )}
+        </StyledErrorDiv>
+      </StyledInputDiv>
+    );
   }
-
-  function onBlurHandler() {
-    setIsFocused(false);
-  }
-
-  return (
-    <StyledInputDiv>
-      <StyledTextDiv>
-        {isFocused && !disabled && (
-          <Text
-            fontSize="xsmall"
-            fontWeight="regular"
-            lineHeight="low"
-            color={error ? "redish" : "purple"}
-          >
-            {label}
-          </Text>
-        )}
-        {!isFocused && value && !error && !disabled && (
-          <Text
-            fontSize="xsmall"
-            fontWeight="regular"
-            lineHeight="low"
-            color="silver"
-          >
-            {label}
-          </Text>
-        )}
-      </StyledTextDiv>
-      <StyledInput
-        type="text"
-        onFocus={onFocusHandler}
-        onBlur={onBlurHandler}
-        $error={error}
-        disabled={disabled}
-        $isFocused={isFocused}
-        {...restProps}
-      />
-
-      <StyledErrorDiv>
-        {error && (
-          <Text
-            fontSize="xsmall"
-            fontWeight="regular"
-            lineHeight="low"
-            color="redish"
-          >
-            {error}
-          </Text>
-        )}
-      </StyledErrorDiv>
-    </StyledInputDiv>
-  );
-}
+);
 export default Input;
