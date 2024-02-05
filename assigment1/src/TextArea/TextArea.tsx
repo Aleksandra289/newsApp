b05363c567565218627aa629e430afd285659405
Aleksandra289 <avujovic737@gmail.com>
2024-02-05 16:06:36 +0100

    post-form-created

---
+++ assigment1/src/TextArea/TextArea.tsx
@@ -0,0 +1,67 @@
+import { StyledTextArea, StyledTextAreaDiv } from "./StyledTextArea";
+import Text from "../components/Text/Text";
+import { useState, TextareaHTMLAttributes, forwardRef, LegacyRef } from "react";
+import { StyledErrorDiv, StyledTextDiv } from "../components/Input/StyledInput";
+interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label: string;
+  error?: string;
+}
+
+const TextArea = forwardRef(
+  (
+    { label, error = "", ...restProps }: TextAreaProps,
+    ref: LegacyRef<HTMLTextAreaElement>
+  ) => {
+    const [isFocused, setIsFocused] = useState(false);
+
+    function onFocusHandler() {
+      setIsFocused(true);
+    }
+
+    function onBlurHandler() {
+      setIsFocused(false);
+    }
+
+    return (
+      <>
+        <StyledTextAreaDiv>
+          <StyledTextDiv>
+            {isFocused && (
+              <Text
+                fontSize="xsmall"
+                fontWeight="regular"
+                lineHeight="low"
+                color={error ? "redish" : "purple"}
+              >
+                {label}
+              </Text>
+            )}
+          </StyledTextDiv>
+          <StyledTextArea
+            placeholder="Description"
+            onFocus={onFocusHandler}
+            onBlur={onBlurHandler}
+            $error={error}
+            $isFocused={isFocused}
+            ref={ref}
+            {...restProps}
+          />
+
+          <StyledErrorDiv>
+            {error && (
+              <Text
+                fontSize="xsmall"
+                fontWeight="regular"
+                lineHeight="low"
+                color="redish"
+              >
+                {error}
+              </Text>
+            )}
+          </StyledErrorDiv>
+        </StyledTextAreaDiv>
+      </>
+    );
+  }
+);
+export default TextArea;
