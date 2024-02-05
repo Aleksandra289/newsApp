7b9887414dfa7bf5e796bc972b091989a1b4c53c
Aleksandra289 <avujovic737@gmail.com>
2024-02-05 09:43:09 +0100

    latest-news-fixed

--- assigment1/src/components/Cards/Card/StyledCard.ts
+++ assigment1/src/components/Cards/Card/StyledCard.ts
@@ -8,8 +8,6 @@ export const StyledCard = styled.div<{
   $padding: `${number}rem`;
 }>`
   position: relative;
-  display: flex;
-  flex-direction: column;
   width: fit-content;
   padding: ${({ $padding }) => $padding};
   border-radius: ${({ theme, $borderRadius }) =>
